6fc2e568e583b11e540676637216ad419bcf9481
PerseusPI <96984641+PerseusPI@users.noreply.github.com>
2022-03-01 23:48:17 +0100

    Update valeur_plus_petite.py

--- valeur_plus_petite.py
+++ valeur_plus_petite.py
@@ -1,12 +1,11 @@
 def trouver_minimum(listeUser:list) -> int or float:
-    listeUser.sort()
-    min = listeUser[len(listeUser) - 1] + 1
+    min = listeUser[0]
     for i in range(0, len(listeUser) - 1):
         if listeUser[i] < min:
             min = listeUser[i]
-            print(f"max: {min}")
+            print(f"min: {min}")
     return min
 
 
-di = [1.5,2.2,0.4,0.9,7.1,1.1,10]
+di = [1.5,2.2,0.4,0.9,7.1,0.2,10]
 print(trouver_minimum(di))
