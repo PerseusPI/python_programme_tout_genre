415e9581896f3b60a58ea5118d07f1fcdb39f630
PerseusPI <96984641+PerseusPI@users.noreply.github.com>
2022-03-10 10:38:48 +0100

    Update extraireExtension.py

--- extraireExtension.py
+++ extraireExtension.py
@@ -1,19 +1,21 @@
-def extraire(extension: list[str], definition: tuple[tuple[str]]) -> str:
+def extraire(extension: list[str], definition: tuple[tuple[str]]) -> list[str]:
     l = [i.split(".") for i in extension]
     d = [i[0].lower() for i in definition]
+    res = []
     for i in range(len(l)):
         if l[i][-1].lower() in d:
             for j in definition:
-              #retirer les lower() si vous voulez exactement le meme nom dans def extensions et vos extensions ici EXE == exe si vous retirer EXE != exe
+                 #retirer les lower() si vous voulez exactement le meme nom dans def extensions et vos extensions ici EXE == exe si vous retirer EXE != exe
                 if j[0].lower() == l[i][-1].lower():
-                    print(f"{extension[i]} ({j[-1]})")
+                    res.append(f"{extension[i]} ({j[-1]})")
         elif not (l[i][-1].lower() in d) and len(l[i]) > 1:
-            print(f"{extension[i]} (Extension Inconnu)")
+            res.append(f"{extension[i]} (Extension Inconnu)")
         else:
-            print(f"{extension[i]} (Aucune Extension)")
+            res.append(f"{extension[i]} (Aucune Extension)")
+    return res
 
 
-extensions = ["notepad.dat", "data.exe", "planning.txt", "vac", "intra.txt", "moc.exe"]
+extensions = ["notepad.doc", "data.exe", "planning", "vac", "intra.txt", "moc.exe"]
 
 definitions_extensions = (
     ("exe", "Executable"),
@@ -22,4 +24,8 @@ definitions_extensions = (
     ("jpeg", "Image JPEG")
 )
 
-extraire(extensions, definitions_extensions)
+res = extraire(extensions, definitions_extensions)
+for i in res:
+    print(i)
+          
+
