f2a3b2b26caa44392ddd7b655864b2e67f8aaa38
PerseusPI <96984641+PerseusPI@users.noreply.github.com>
2022-03-15 19:07:53 +0100

    Update classePersonne.py

--- classePersonne.py
+++ classePersonne.py
@@ -1,21 +1,33 @@
 class Personne:
-    def __init__(self, nom: str, age: int):
+    def __init__(self, nom: str = "", age: int = 0):
         self.nom = nom
+        if nom == "":
+            self.nom = self.demanderNom()
         self.age = age
         print("Clonage de la personne")
 
 
+    def demanderNom(self):
+        nameUser = input("Votre nom : ")
+        return nameUser
+
+
     def SePresenter(self):
-        print(f"Je m'appelle {self.nom} et j'ai {self.age}")
-        if self.estMajeur():
-            print("Je suis majeur")
+        if self.age == 0:
+            print(f"Je m'appelle {self.nom}")
         else:
-            print("Je suis mineur")
+            print(f"Je m'appelle {self.nom} et j'ai {self.age} ans")
+            if self.estMajeur():
+                print("je suis majeur")
+            else:
+                print("Je suis mineur")
+
     
-    
+
     def estMajeur(self):
         return self.age >= 18
 
 
-personne1 = Personne("Sam", 17)
+personne1 = Personne("",18)
 personne1.SePresenter()
+
