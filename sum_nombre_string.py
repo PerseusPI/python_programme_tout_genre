2cd86889d6283ea2ed1c1ad65210fc758b54ae14
PerseusPI <96984641+PerseusPI@users.noreply.github.com>
2022-03-16 19:29:09 +0100

    Update sum_nombre_string.py

--- sum_nombre_string.py
+++ sum_nombre_string.py
@@ -1,6 +1,6 @@
 def sum_string(k: str) -> int:
-    return sum(int(i.strip(".")) for i in k.split() if i.strip(".").isdigit())
+    return sum(int(i.strip(".")) or int(i.strip(",")) for i in k.split() if i.strip(".").isdigit() or i.strip(",").isdigit())
 
 
-k = "La première usine dispose de 324 employés. La deuxième usine en compte 529. L'unité de Los Angeles dispose quant à elle de 918 employés."
+k = "La première usine dispose de 32469 employés. La deuxième usine en compte 529. L'unité de Los Angeles dispose quant à elle de 918 employés."
 print(sum_string(k))
