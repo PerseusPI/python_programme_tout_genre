a7865710d03f328cf68457c1e078e13ed0982487
PerseusPI <96984641+PerseusPI@users.noreply.github.com>
2022-03-18 14:13:49 +0100

    Update rechercheDichotomique.py

--- rechercheDichotomique.py
+++ rechercheDichotomique.py
@@ -1,19 +1,17 @@
-def dichotomie(nbreATrouver:int, liste: list[int]) -> bool:
-    liste.sort()
+def dichotomie(a: int, l: list) -> bool:
+    l.sort()
     debut = 0
-    fin = len(liste) - 1
+    fin = len(l) - 1
     milieu = 0
-    trouver = False
-    while debut <= fin and trouver == False:
+    while debut <= fin:
         milieu = (debut + fin) // 2
-        if liste[milieu] == nbreATrouver:
-            trouver = True
-        elif liste[milieu] > nbreATrouver:
+        if l[milieu] == a:
+            return True
+        elif l[milieu] > a:
             fin = milieu - 1
             milieu = (debut + fin) // 2
         else:
             debut = milieu + 1
             milieu = (debut + fin) // 2
-    return trouver
-
-print(dichotomie(4, [1,2,3,4,8]))
+    return False
+print(dichotomie(9,[1,4,3,2,9]))
